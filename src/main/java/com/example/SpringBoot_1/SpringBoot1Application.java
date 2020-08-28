d637b5892b5345243e59afe8427bd9bac935c707
TridentMark <2411kjn@gmail.com>
2020-08-28 17:24:18 +0700

    Initial commit

---
+++ src/main/java/com/example/SpringBoot_1/SpringBoot1Application.java
@@ -0,0 +1,13 @@
+package com.example.SpringBoot_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringBoot_1/SpringBoot1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBoot_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
